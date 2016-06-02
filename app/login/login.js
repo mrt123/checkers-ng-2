c54267c5ffce98dc1e71ba109f7fb3a0f0ff4532
Tomasz <tomasz.szawara@gmail.com>
2016-06-02 11:40:36 +0100

    Login screen

---
+++ app/login/login.js
@@ -0,0 +1,28 @@
+angular
+    .module('app.LoginCtrl', [])
+    .controller('LoginCtrl', function ($scope, account, $timeout, $state, facebook) {
+
+        $scope.loginWithFacebook = loginWithFacebook;
+        $scope.login = login;
+
+        function loginWithFacebook() {
+            facebook.login(function (response) {
+                loginSuccess();
+            });
+        }
+
+        function login(email, password) {
+            account.signIn(email, password, loginSuccess, showError);
+        }
+        
+        function loginSuccess() {
+            $state.go('home');
+        }
+
+        function showError(error) {
+            $timeout(function () {   // avoid existing digest
+                $scope.error = error.message;
+            });
+        }
+    });
+

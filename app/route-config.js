28a1f1acbdbcf8a4d96f1672dd897555e34424d2
Tomasz <tomasz.szawara@gmail.com>
2016-06-05 18:54:37 +0100

    removed redundant states and markup

--- app/route-config.js
+++ app/route-config.js
@@ -23,22 +23,5 @@ function config($stateProvider) {
             url: '/login',
             templateUrl: 'login/login.html',
             controller: 'LoginCtrl'
-        })
-        .state('user.game', {
-            url: '/game',
-            views: {
-                'game': {
-                    templateUrl: 'game/game.html',
-                    controller: 'GameCtrl'
-                }
-            }
-        })
-        .state('user.game.board', {  // not materialised in url
-            views: {
-                'board': {
-                    templateUrl: 'board/board.html',
-                    controller: 'BoardCtrl'
-                }
-            }
         });
 }

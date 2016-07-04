ef032daefa4c6864777bd6169a226ab3970cc652
Tomasz <tomasz.szawara@gmail.com>
2016-07-04 17:55:10 +0100

    AbstractAccount : reverted back to prototype method definition ( decrease memory use, improve performance)

--- app/common/account/AbstractAccount.js
+++ app/common/account/AbstractAccount.js
@@ -3,40 +3,34 @@ angular
     .factory('AbstractAccount', AbstractAccount);
 
 function AbstractAccount() {
-    
-    return function() {
-        
-        function Constructor(name) {
-            this.name = name;
-            this.signUpSuccessCallbacks = [];
-            this.logInSuccessCallbacks = [];
-            this.addOnSignUpCallback = addOnSignUpCallback;
-            this.addOnLoginCallback = addOnLoginCallback;
-            this.executeSignUpSuccessCallbacks = executeSignUpSuccessCallbacks;
-            this.executeLoginSuccessCallbacks = executeLoginSuccessCallbacks;
-        }
-        return Constructor;
-    }();
 
-    function addOnSignUpCallback(callback) {
+    function Constructor(name) {
+        this.name = name;
+        this.signUpSuccessCallbacks = [];
+        this.logInSuccessCallbacks = [];
+    }
+
+    Constructor.prototype.addOnSignUpCallback = function(callback) {
         this.signUpSuccessCallbacks.push(callback);
-    }
-    
-    function addOnLoginCallback(callback) {
+    };
+
+    Constructor.prototype.addOnLoginCallback = function(callback) {
         this.logInSuccessCallbacks.push(callback);
-    }
+    };
 
-    function executeSignUpSuccessCallbacks(args) {
-        executeCallbacks(this.signUpSuccessCallbacks, args);
-    }
+    Constructor.prototype.executeSignUpSuccessCallbacks = function(args) {
+        this.executeCallbacks(this.signUpSuccessCallbacks, args);
+    };
 
-    function executeLoginSuccessCallbacks(args) {
-        executeCallbacks(this.logInSuccessCallbacks, args);
-    }
+    Constructor.prototype.executeLoginSuccessCallbacks = function(args) {
+        this.executeCallbacks(this.logInSuccessCallbacks, args);
+    };
 
-    function executeCallbacks(callbacks, args) {
+    Constructor.prototype.executeCallbacks = function(callbacks, args) {
         angular.forEach(callbacks, function (callback) {
             callback.apply(undefined, args);
         });
-    }
+    };
+
+    return Constructor;
 }

95738aef04537edc515730244c0e92b1cf3cf8b2
Tomasz <tomasz.szawara@gmail.com>
2016-08-03 21:03:47 +0100

    intercept post game request , refactored gulpBackend task

--- other/tools/backend/parseServer.js
+++ other/tools/backend/parseServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var app = express();
 
-var api = new ParseServer({
+var parseServer = new ParseServer({
     databaseURI: 'mongodb://localhost:27017/dev',
     appId: 'CHECKERS_2',
     masterKey: 'MASTER_KEY', // Keep this key secret!
@@ -10,7 +10,12 @@ var api = new ParseServer({
 });
 
 // Serve the Parse API on the /parse URL prefix
-app.use('/parse', api);
+app.use('/parse', parseServer);
+
+app.post('/parse/classes/Game', function (req, res, next) {
+    console.log('Creating a game  ...');
+    next(); // pass control to the next handler
+});
 
 app.listen(1337, function() {
     console.log('parse-server-example running on port 1337.');
--- other/tools/gulp-tasks/gulpBackend.js
+++ other/tools/gulp-tasks/gulpBackend.js
@@ -4,7 +4,7 @@ var gulp_express = require('gulp-express');
 var Project = require('./Project');
 var project = new Project();
 
-gulp.task('backendServer', function () {
-    gulp_express.run([project.BACKEND_PATH + '/parseAPI.js'], {}, false);
+gulp.task('parseServer', function () {
+    gulp_express.run([project.BACKEND_PATH + '/parseServer.js'], {}, false);
     gulp.watch([project.BACKEND_PATH + '/**/*.js'], [gulp_express.run]);
 });

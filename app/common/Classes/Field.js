6a2996dc7b45acad568393da0fd15a3cf7402262
Tomasz <tomasz.szawara@gmail.com>
2016-08-07 09:48:09 +0100

    implemented field colors

--- app/common/Classes/Field.js
+++ app/common/Classes/Field.js
@@ -8,6 +8,7 @@ function Field() {
         this.number = number;
         this.rowNumber = rowNumber;
         this.columnNumber = columnNumber;
+        this.color = this.getColor(rowNumber, number);
         this.pin = undefined;
     };
 
@@ -25,6 +26,23 @@ function Field() {
 
     Field.prototype.removePin = function () {
         this.pin = undefined;
+    };
+
+    Field.prototype.isColorWhite = function(rowNo, fieldNo) {
+        var eveRow = rowNo % 2 == 0;
+        var oddRow = !eveRow;
+        var evenNumber = fieldNo % 2 == 0;
+        var oddNumber = !evenNumber;
+        return (oddRow && oddNumber ) || (eveRow && evenNumber);
+    };
+    
+    Field.prototype.getColor = function(rowNo, fieldNo) {
+        if(this.isColorWhite(rowNo, fieldNo)){
+            return 'white';
+        }
+        else {
+            return 'black';
+        }
     };
 
     return Field;

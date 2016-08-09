64bfb5e36889dc689bab4345c05bd2a1e6f13ca8
Tomasz <tomasz.szawara@gmail.com>
2016-08-09 22:35:42 +0100

    basic debug mode, moved from source to dev

--- other/tools/_devAPI.js
+++ other/tools/_devAPI.js
@@ -1,18 +1,31 @@
-var _dev = (function () {
+setTimeout(function () {
     
-    var _dev = {
-        debug: false,
-        getService: function (serviceName) {
-            return angular.element(document.body).injector().get(serviceName)
-        },
-        getState: function () {
-            return this.getService('$state')
-        }
-    };
+    window._dev = getDevObject();
 
-    startReloadCounter(new Date(), createDevElement());
+    startReloadCounter(new Date(), createReloadEl());
 
-    function createDevElement() {
+    appendDebugDOM();
+    
+    function getDevObject() {
+        return {
+            debug: true,
+            getService: function (serviceName) {
+                return angular.element(document.body).injector().get(serviceName)
+            },
+            getState: function () {
+                return this.getService('$state')
+            }
+        };
+    }
+
+    function startReloadCounter(reloadDate, el) {
+        setInterval(function () {
+            var parsedSeconds = getDiffInSeconds(new Date(), reloadDate);
+            el.children().text('Reloaded ' + parsedSeconds + ' seconds ago.');
+        }, 1000);
+    }
+
+    function createReloadEl() {
         // wrapping in extra div avoids chrome DOM inspector performance issues.
         var devEl = angular.element('<div><div id="ch-dev"></div>');
 
@@ -23,9 +36,16 @@ var _dev = (function () {
             opacity: '0.5',
             'font-size': '16px'
         });
-        
+
         angular.element(document.querySelector('html')).append(devEl);
         return devEl;
+    }
+
+    function appendDebugDOM() {
+        var squares = $('ch-square');
+        generateDebugInfoOnElements(squares, squareViewUpdater);
+        var pins = $('ch-pin');
+        generateDebugInfoOnElements(pins, pinViewUpdater);
     }
 
     function getDiffInSeconds(date1, date2) {
@@ -34,22 +54,38 @@ var _dev = (function () {
         return parseInt(diffInSeconds);
     }
 
-    function startReloadCounter(reloadDate, el) {
-        setInterval(function () {
-            var parsedSeconds = getDiffInSeconds(new Date(), reloadDate);
-            el.children().text('Reloaded ' + parsedSeconds + ' seconds ago.');
-        }, 1000);
+    function generateDebugInfoOnElements(elements, viewUpdater) {
+
+        for (var i = 0; i < elements.length; i++) {
+            var squareMatrixEl = elements[i];
+            viewUpdater(squareMatrixEl);
+        }
     }
 
-    return _dev;
-})();
+    function pinViewUpdater(el) {
+        var newDiv = $('<div>', {
+            'class': 'debug',
+            css: {
+                color: 'red',
+                'text-align': 'center',
+                'line-height': '60px'
+            }
+        });
 
-/**
- * TODO: inject numbers to fields and discs in debug mode
- *      $('ch-square')     attr: number="31"
- *      $('ch-pin')     attr:   id="7"
- *      
- *      TODO: remove debug code from ch-pin , ch-square , controllers and templates
- */
+        return newDiv
+            .text(el.getAttribute('id'))
+            .appendTo(el);
+    }
 
+    function squareViewUpdater(el) {
+        var newDiv = $('<div>', {
+            'class': 'debug'
+        });
 
+        return newDiv
+            .text(el.getAttribute('number'))
+            .appendTo($(el).find('.matrix'));
+    }
+}, 1);
+
+// TODO: hold debug state in local storage

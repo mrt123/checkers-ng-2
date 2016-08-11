fdc89c07790a79bb6181af936c9f861648ff86f6
Tomasz <tomasz.szawara@gmail.com>
2016-08-11 22:33:14 +0100

    ported to ES6

--- app/common/Classes/Board.js
+++ app/common/Classes/Board.js
@@ -4,120 +4,124 @@ angular
 
 function Board(Field, Pin) {
 
-    var Board = function () {
-        this.fields = [];
-        this.pins = [];
-    };
-
-    Board.prototype.init = function () {
-        this.fields = this._generateFields();
-        return this;
-    };
-
-    Board.prototype.initFromPlainFieldsObjects = function(objectsArray) {
-       this.fields =  this._generateFieldsFromObjects(objectsArray);
-        return this;
-    };
-
-    /**
-     * WHITES always starts from 1.
-     * If you're WHITE!!!, your rendering engine will invert field order.
-     */
-    Board.prototype._generateFields = function () {
-        var fields = [];
-
-        for (var fieldNumber = 1; fieldNumber <= 64; fieldNumber++) {
-            var field = new Field(fieldNumber);
-            this._addPinToFieldWhenNeeded(field);
-            fields.push(field);
+    class Board {
+        constructor() {
+            this.fields = [];
+            this.pins = [];
         }
-        return fields;
-    };
-    
-    Board.prototype._generateFieldsFromObjects = function (objectsArray) {
-        var fields = [];
 
-        for (var i = 0; i < objectsArray.length; i++) {
-            var json = objectsArray[i];
-            var field = Field.fromPlainObject(json);
-            fields.push(field);
+        init() {
+            this.fields = this._generateFields();
+            return this;
         }
-        return fields;
-    };
 
-    Board.prototype._addPinToFieldWhenNeeded = function (field) {
+        initFromPlainFieldsObjects(objectsArray) {
+            this.fields = this._generateFieldsFromObjects(objectsArray);
+            return this;
+        }
 
-        if(field.color === 'black') {
-            // PLAYER 1 fields
-            if (field.number >= 41) {
-                this._addPinToField(field, 'black');
+        /**
+         * WHITES always starts from 1.
+         * If you're WHITE!!!, your rendering engine will rotate board.
+         * This way only 1 view of the board needs to be persisted, and GameMaster can make decisions,
+         * based on field numbers and player color.
+         */
+        _generateFields() {
+            var fields = [];
+
+            for (var fieldNumber = 1; fieldNumber <= 64; fieldNumber++) {
+                var field = new Field(fieldNumber);
+                this._addPinToFieldWhenNeeded(field);
+                fields.push(field);
             }
+            return fields;
+        }
 
-            // PLAYER 2 fields
-            if (field.number <= 24) {
-                this._addPinToField(field, 'white');
+        _generateFieldsFromObjects(objectsArray) {
+            var fields = [];
+
+            for (var i = 0; i < objectsArray.length; i++) {
+                var json = objectsArray[i];
+                var field = Field.fromPlainObject(json);
+                fields.push(field);
+            }
+            return fields;
+        }
+
+        _addPinToFieldWhenNeeded(field) {
+
+            if (field.color === 'black') {
+                // PLAYER 1 fields
+                if (field.number >= 41) {
+                    this._addPinToField(field, 'black');
+                }
+
+                // PLAYER 2 fields
+                if (field.number <= 24) {
+                    this._addPinToField(field, 'white');
+                }
             }
         }
-    };
-    
-    Board.prototype.movePinToField = function (pin, targetField) {
-        var baseField = this.getFieldByPin(pin);
-        baseField.removePin();
-        targetField.setPin(pin);
-        return targetField;
-    };
 
-    Board.prototype.getFieldByPin = function (pin) {
-        return this.fields.filter(function (field) {
-            if(field.hasPin()) {
-                return field.pin.id === pin.id;
+        movePinToField(pin, targetField) {
+            var baseField = this.getFieldByPin(pin);
+            baseField.removePin();
+            targetField.setPin(pin);
+            return targetField;
+        }
+
+        getFieldByPin(pin) {
+            return this.fields.filter(function (field) {
+                if (field.hasPin()) {
+                    return field.pin.id === pin.id;
+                }
+                else {
+                    return false;
+                }
+            })[0];
+        }
+
+        isDiagonalField(baseField, targetField, playerColor) {
+            var areFieldsInNeighbourColumns = this.areFieldsInNeighbourColumns(baseField, targetField, playerColor);
+            var isTargetForward = this.isForwardField(baseField, targetField, playerColor);
+            return areFieldsInNeighbourColumns && isTargetForward;
+        }
+
+        areFieldsInNeighbourColumns(field1, field2) {
+            var columnDiff = Math.abs(field1.columnNumber - field2.columnNumber);
+            return columnDiff === 1;
+        }
+
+        isForwardField(baseField, targetField, playerColor) {
+            var diffToNewRow = targetField.rowNumber - baseField.rowNumber;
+
+            if (playerColor === 'white') {
+                return diffToNewRow === 1;
             }
             else {
-                return false;
+                return diffToNewRow === -1;
             }
-        })[0];
-    };
-
-    Board.prototype.isDiagonalField = function (baseField, targetField, playerColor) {
-        var areFieldsInNeighbourColumns = this.areFieldsInNeighbourColumns(baseField, targetField, playerColor);
-        var isTargetForward = this.isForwardField(baseField, targetField, playerColor);
-        return areFieldsInNeighbourColumns && isTargetForward;
-    };
-
-    Board.prototype.areFieldsInNeighbourColumns = function (field1, field2) {
-        var columnDiff = Math.abs(field1.columnNumber - field2.columnNumber);
-        return columnDiff === 1;
-    };
-
-    Board.prototype.isForwardField = function (baseField, targetField, playerColor) {
-        var diffToNewRow = targetField.rowNumber - baseField.rowNumber;
-
-        if (playerColor === 'white') {
-            return diffToNewRow === 1;
         }
-        else {
-            return diffToNewRow === -1;
-        }
-    };
 
-    Board.prototype._addPinToField = function (field, color) {
-        var pin = new Pin(color, this.pins.length);
-        field.setPin(pin);
-        this.pins.push(pin);
-    };
-
-    Board.prototype.toPlainObject = function() {
-        var plainFieldObjects = [];
-
-        for (var i = 0; i < this.fields.length; i++) {
-            var nativeField = this.fields[i];
-            plainFieldObjects.push(nativeField.toPlainObject());
+        _addPinToField(field, color) {
+            var pin = new Pin(color, this.pins.length);
+            field.setPin(pin);
+            this.pins.push(pin);
         }
-        
-        return {
-            fields: plainFieldObjects
+
+        toPlainObject() {
+            var plainFieldObjects = [];
+
+            for (var i = 0; i < this.fields.length; i++) {
+                var nativeField = this.fields[i];
+                plainFieldObjects.push(nativeField.toPlainObject());
+            }
+
+            return {
+                fields: plainFieldObjects
+            }
         }
-    };
+    }
 
     return Board;
 }
